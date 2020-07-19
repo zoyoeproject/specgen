76840b768d41c1de5b7f4fd962d9673035da516e
zkp@zoyoe.com <zkp@zoyoe.com>
2020-07-19 15:34:15 +0800

    clarify llvm-parser code structure a bit

--- llvm/specbuilder.ml
+++ llvm/specbuilder.ml
@@ -3,48 +3,62 @@ type name =
   | Name of string
 
 let string_of_name na = match na with
-  | Anonymous -> false
+  | Anonymous -> assert false
   | Name str -> str
+
+let mkName stropt = match stropt with
+  | None -> Anonymous
+  | Some str -> Name str
 
 type c_type =
   | Void
   | Int
-  | Ptr of c_type
-  | Struct of name * (name * c_type array)
-  | Function of name * (name * c_type array)
+  | Ref of c_type
+  | Struct of name
+  | Function of name
 
-let string_of_ctype ctype name_only =
+let rec indicator_of ctype =
   match ctype with
-  | Void -> "unit"
+  | Void -> "Unit"
+  | Int -> "Int"
+  | Ref ctype -> "Ref " ^ indicator_of ctype
+  | Struct na -> "T_" ^ string_of_name na
+  | Function na -> "F_" ^ string_of_name na
+
+let rec coq_type ctype =
+  match ctype with
+  | Void -> "void"
   | Int -> "Z"
-  | Ptr ctype -> "ref " ^ string_of_ctype ctype true
-  | Struct (na, fields) -> string_of_name na
-  | Function _ -> "function"
+  | Ref ctype -> "reference " ^ coq_type ctype
+  | Struct na -> string_of_name na ^ ".t"
+  | Function na -> string_of_name na ^ ".body"
 
-exception UnsupportType of string
+exception UnsupportType of Llvm.lltype
 
-let parse_type llty name_only =
-  match llty with
+let rec parse_type llty =
+  match Llvm.classify_type llty with
   | Llvm.TypeKind.Integer  -> Int
-  | Llvm.TypeKind.Pointer  -> mkPtr (parse_type (Llvm.element_type llty) true)
-  | Llvm.TypeKind.Array -> mkPtr (parse_type (Llvm.element_type llty) true)
-  (* | Llvm.TypeKind.Struct -> mkStruct *)
-  | _  -> raise UnsupportType llty
+  | Llvm.TypeKind.Pointer  -> Ref (parse_type (Llvm.element_type llty))
+  | Llvm.TypeKind.Array -> Ref (parse_type (Llvm.element_type llty))
+  | Llvm.TypeKind.Struct -> Struct (mkName (Llvm.struct_name llty))
+  | _  -> raise (UnsupportType llty)
 
-let print_func_def func =
+let emit_func_head lv =
+  (* name should be used for file name *)
+  (* let func_name = Llvm.value_name lv; *)
   Printf.printf "Definition\n";
-  Printf.printf "  name %s\n" (Llvm.value_name lv);
   let func_ty = Llvm.type_of lv in
+  let ptypes = Llvm.param_types func_ty in
+  let rettyp = Llvm.return_type func_ty in
+  Printf.printf "%s : %s :=\n"
+    (Array.fold_left (fun acc llty ->
+      let typ = parse_type llty in
+      let coqtyp = coq_type typ in
+      acc ^ " " ^ coqtyp
+    ) "body " ptypes)
+    (coq_type (parse_type rettyp))
 
-let print_val lv =
-  Printf.printf "Value\n" ;
-  Printf.printf "  name %s\n" (Llvm.value_name lv) ;
-  let llty = Llvm.type_of lv in
-  Printf.printf "  type %s\n" (Llvm.string_of_lltype llty) ;
-  print_type llty ;
-  ()
-
-let print_fun lv =
+let emit_func_body lv =
   Llvm.iter_blocks
     (fun llbb ->
       Printf.printf "  bb: %s\n" (Llvm.value_name (Llvm.value_of_block (llbb))) ;
@@ -56,19 +70,25 @@ let print_fun lv =
     )
     lv
 
+let emit_fun lv =
+  emit_func_head lv;
+  emit_func_body lv
+
 let _ =
   let llctx = Llvm.global_context () in
   let llmem = Llvm.MemoryBuffer.of_file Sys.argv.(1) in
   let llm = Llvm_bitreader.parse_bitcode llctx llmem in
-  (*Llvm.dump_module llm ;*)
 
-  Printf.printf "*** iter_functions ***\n" ;
-  Llvm.iter_functions print_val llm ;
+  (* Uncomment this for debugging purpose
+   * Llvm.dump_module llm;
+   *)
 
-  Printf.printf "*** basic blocks/instructions ***\n" ;
-  Llvm.iter_functions print_fun llm ;
+  Printf.printf "*** iter_functions ***\n";
+  Llvm.iter_functions emit_fun llm;
 
-  Printf.printf "*** iter_globals ***\n" ;
-  Llvm.iter_globals print_val llm ;
+  (*
+  Printf.printf "*** iter_globals ***\n";
+  Llvm.iter_globals print_val llm;
+  *)
 
   ()
