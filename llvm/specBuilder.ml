c1b62188652635067685881bb433b803bda30778
xgao@zoyoe.com <xgao@zoyoe.com>
2021-01-28 21:08:00 +0800

    add specbuilder.ml

---
+++ llvm/specBuilder.ml
@@ -0,0 +1,20 @@
+let _ =
+  let llctx = Llvm.global_context () in
+  let llmem = Llvm.MemoryBuffer.of_file Sys.argv.(1) in
+  let llm = Llvm_bitreader.parse_bitcode llctx llmem in
+
+  (* Uncomment this for debugging purpose
+   * Llvm.dump_module llm;
+   *)
+
+  Llvm.iter_functions CfgBuilder.emit_llfun llm;
+
+  let emitter = Codeflow.Emitter.mkEmitter () in
+  Lltrans.TypeBuilder.emit_types emitter;
+
+  (*
+  Printf.printf "*** iter_globals ***\n";
+  Llvm.iter_globals print_val llm;
+  *)
+
+  ()
